90cc7d38915b8aa1feed413ca83e7837329b0a7c
toya08 <choconoricherry0814@icloud.com>
2026-01-29 12:28:48 +0900

    Fix gunicorn command in entrypoint.sh to use correct module path

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export PYTHONPATH=./vendor
 python3 manage.py migrate --noinput
-python3 -m gunicorn --workers 2 apprunner_0129 .wsgi --bind 0.0.0.0:8000
+python3 -m gunicorn --workers 2 python_apps_django.wsgi --bind 0.0.0.0:8000
